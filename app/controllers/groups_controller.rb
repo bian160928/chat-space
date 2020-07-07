ea7a8ebdba2d83aed708cad56b8ffeab28185858
bian160928 <biankobayashi@gmail.com>
2020-07-07 14:19:20 +0900

    make_up_group

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,37 @@
+class GroupsController < ApplicationController
+  def index
+  end
+
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループ名を作成しました"
+    else
+      render :new
+    end
+  end
+
+  def  edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root "messages#index"
+  root 'groups#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
