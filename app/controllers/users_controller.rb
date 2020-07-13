3e1bf72b4ec7d3868c86fc18d13e2e1da11e6dd8
bian160928 <biankobayashi@gmail.com>
2020-07-13 20:54:15 +0900

    Incremental_search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def index
+    return nil if params[:keywoed] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+  
   def edit
   end
 
