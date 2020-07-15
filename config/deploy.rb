617f6511ac36c4d17e55b22b8b3d397a8a117362
bian160928 <biankobayashi@gmail.com>
2020-07-15 18:15:52 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock "3.14.1"
 
 set :application, "chat-space"
 set :repo_url, "git@github.com:bian160928/chat-space.git"
